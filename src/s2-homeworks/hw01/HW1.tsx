9cef9650d3e43a614ea375fada8cc7e4b824845c
Ateo101 <nikmail242@gmail.com>
2022-11-17 14:13:19 +0100

    HW2 - #1-11

--- src/s2-homeworks/hw01/HW1.tsx
+++ src/s2-homeworks/hw01/HW1.tsx
@@ -8,9 +8,9 @@ import avaSynthwave from './ava-synthwave.png'
 
 /*
 * 1 - описать тип MessageType ✓
-* 2 - описать тип MessagePropsType в файле Message.tsx
-* 3 - в файле Message.tsx отобразить приходящие данные
-* 4 - выполнить пункты 2, 3 в файле FriendMessage.tsx
+* 2 - описать тип MessagePropsType в файле Message.tsx ✓
+* 3 - в файле Message.tsx отобразить приходящие данные ✓
+* 4 - выполнить пункты 2, 3 в файле FriendMessage.tsx ✓
 * 5 - сделать стили в соответствии с дизайном
 * */
 
